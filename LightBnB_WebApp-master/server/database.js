a47cb9d79664754ed9034524bca328ba62df56bc
Farid Asadpour <yani5752000@yahoo.com>
2021-07-23 02:17:40 +0000

    create listings

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -141,9 +141,34 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const queryParams = [ 
+  property["title"], 
+  property["description"], 
+  property["owner_id"],
+  property["cover_photo_url"],
+  property["thumbnail_photo_url"], 
+  property["cost_per_night"], 
+  property["parking_spaces"], 
+  property["number_of_bathrooms"], 
+  property["number_of_bedrooms"],
+  property["province"], 
+  property["city"],
+  property["country"], 
+  property["street"], 
+  property["post_code"]
+  ];
+  // 2
+  let queryString = `
+  INSERT INTO properties (
+    title, description, owner_id, cover_photo_url, thumbnail_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, province, city, country, street, post_code) 
+    VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *;
+  `;
+
+  
+  // 5
+  console.log(queryString, queryParams);
+
+  // 6
+  return pool.query(queryString, queryParams).then((res) => res.rows);
 }
 exports.addProperty = addProperty;
